831f23cb012e3dcdd49cec911fc5d037f2305cc1
Josué <d4n11083@estudiantec.cr>
2021-04-14 11:48:45 -0600

    Comentarios varios fragmento descripcion producto
    Explicando cosas del fragmento de la descripción

--- app/src/main/java/com/example/supermercadotico/Fragments/DescripcionProductoFragment.java
+++ app/src/main/java/com/example/supermercadotico/Fragments/DescripcionProductoFragment.java
@@ -19,61 +19,75 @@ import com.example.supermercadotico.R;
 import com.like.LikeButton;
 import com.like.OnLikeListener;
 
-
+/**
+ * Fragmento para cargar la descripción de un producto
+ * Implementa OnLikeListener, para el botón de agregar al carrito.
+ * El fragmento de Productos, le pasa un objeto producto a este view para cargarlo en pantalla
+ */
 public class DescripcionProductoFragment extends Fragment implements OnLikeListener {
+    //Tag para los logs y saber de donde se están haciendo
     private static final String TAG = "DescripcionProductoFrag";
 
     //Variables
+    //Objeto donde se va a cargar la información que el cliente seleccionó del fragment pasado
     private Producto mProducto;
 
     //widgets
-    private TextView mNombreProducto, mPrecio,mStock, mDescripcion, mCategoria, mProveedor;
-    private LikeButton mBotonAgregarCarrito;
-    private RelativeLayout mBotonAtras;
-    private ImageView mImagenProducto;
+    private TextView mNombreProducto, mPrecio,mStock, mDescripcion, mCategoria, mProveedor;  //TextViews de todos los campos del producto
+    private LikeButton mBotonAgregarCarrito;   //Boton de like que hace una animación cuando se presiona
+    private RelativeLayout mBotonAtras;      //Botón de la flecha de la barra de arriba
+    private ImageView mImagenProducto;      //Imagen del producto
 
+    //Se carga el bundle en el onCreate, por que OnCreate se ejecuta primero que OnCreate view,
+    //para que cuando se busquen los componentes ya el bundle con el objeto de producto ya este cargado
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //El bundle es lo que permite que el objeto producto puede enviarse desde el fragmento anterior a este,
+        //por eso es parcelable
         Bundle bundle = this.getArguments();
         if(bundle!=null){
-            mProducto = bundle.getParcelable(getString(R.string.intent_producto));
+            mProducto = bundle.getParcelable(getString(R.string.intent_producto));  //Acá toma el objeto
             Log.d(TAG, "onCreate: Tengo el producto que seleccionó el cliente" + mProducto.getNombre());
         }
     }
 
+    //Acá se asignan los componentes a los objetos.. se conectan  con el layout
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Infla la vista con este fragmento
         View view = inflater.inflate(R.layout.fragment_descripcion_producto,container,false);
         Log.d(TAG, "onCreateView: started.");
 
-        //Buscando los componentes
-        mNombreProducto = view.findViewById(R.id.nombreProducto_desc);
-        mPrecio = view.findViewById(R.id.precio_desc);
-        mStock = view.findViewById(R.id.stock_desc);
-        mCategoria = view.findViewById(R.id.categoria_desc);
-        mProveedor = view.findViewById(R.id.proveedor_desc);
-        mDescripcion = view.findViewById(R.id.descripcion_desc);
-        mBotonAgregarCarrito = view.findViewById(R.id.agregar_carrito_desc);
-        mBotonAtras = view.findViewById(R.id.boton_atras_desc);
-        mImagenProducto = view.findViewById(R.id.imagen_Producto_desc);
+        //Buscando los componentes y los conecta con los objetos de este fragmento
+        mNombreProducto = view.findViewById(R.id.nombreProducto_desc);  //Nombre del producto
+        mPrecio = view.findViewById(R.id.precio_desc);                  //Precio del Producto
+        mStock = view.findViewById(R.id.stock_desc);                    //Cantidad en el inventario
+        mCategoria = view.findViewById(R.id.categoria_desc);           //Ctegoría del producto
+        mProveedor = view.findViewById(R.id.proveedor_desc);            //Proveedor del producto
+        mDescripcion = view.findViewById(R.id.descripcion_desc);        //DEscripción del producto
+        mBotonAgregarCarrito = view.findViewById(R.id.agregar_carrito_desc); //Boton de "like" para agregar al carrito
+        mBotonAtras = view.findViewById(R.id.boton_atras_desc);         //Boton de la flecha para ir al fragment anterior
+        mImagenProducto = view.findViewById(R.id.imagen_Producto_desc);  //Imagen del producto
 
-        mBotonAgregarCarrito.setOnLikeListener(this);
+        mBotonAgregarCarrito.setOnLikeListener(this);       //Listener del botón de like
 
-        init();
+        init();             //Inicializa carga la infromación a los componentes.
 
         return view;
     }
 
+    //Se inicializa, o sea se carga toda la info del Producto en el layout
     private void init(){
         Log.d(TAG, "init: Initializing" + getString(R.string.tag_user_fragment_descripcion_producto));
-        if (mProducto !=null){
+
+        //Si el producto no es null, osea no le llegó el produto del fragment anterior
+        if (mProducto !=null){ //Si le llegó el Producto
             Glide.with(getActivity())
                     .load(mProducto.getImagen_Producto())
-                    .into(mImagenProducto);
-            mNombreProducto.setText(mProducto.getNombre());
+                    .into(mImagenProducto);   //Le pone la imagen
+            mNombreProducto.setText(mProducto.getNombre());  //Carga el nombre y los datos a los componentes
             mPrecio.setText(mProducto.getPrecio());
             mCategoria.setText(mProducto.getCategoria());
             mDescripcion.setText(mProducto.getDescripcion());
@@ -82,11 +96,13 @@ public class DescripcionProductoFragment extends Fragment implements OnLikeListe
         }
     }
 
+    //Cuando se presiona el botón de like y queda de color verde
     @Override
     public void liked(LikeButton likeButton) {
         Log.d(TAG, "Agregado al Carrito: ");
     }
 
+    //Cuando se vuelve a presionar para quitarle el like, queda de color negro. 
     @Override
     public void unLiked(LikeButton likeButton) {
         Log.d(TAG, "Eliminado del Carrito: ");
